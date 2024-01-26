f4be764203e28e0b9b3f46d64d5edd16ea1b9e46
TheOnlyWayUp <thedarkdraconian@gmail.com>
2024-01-26 13:16:56 +0000

    feat(utils): Add build_url, fetch_url utils. Update headers

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
+from typing import Optional
+import aiohttp
 from pydantic import BaseModel
+
+base_headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0"
+}
 
 
 def get_fields(model: BaseModel) -> list[str]:
@@ -10,3 +16,64 @@ def get_fields(model: BaseModel) -> list[str]:
         else:
             attribs.append(name)
     return attribs
+
+
+def construct_fields(fields: dict) -> str:
+    """Construct a fields query string from a dictionary.
+
+    Example:
+    >>> d: StoryModelFieldsType = {
+        "tags": True,
+        "id": True,
+        "parts": {"id": True},
+        "tagRankings": True
+    }
+    >>> construct_fields(d)
+    'tags,id,parts(id),tagRankings'"""
+    fields_str = ""
+
+    for key, value in fields.items():
+        if value is False:
+            continue
+
+        if value is True:
+            fields_str += f"{key},"
+
+        if type(value) is dict:
+            fields_str += f"{key}({construct_fields(value)}),"
+
+    fields_str = fields_str.removesuffix(",")
+
+    return fields_str
+
+
+def build_url(
+    path: str,
+    fields: Optional[dict] = None,
+    limit: Optional[int] = None,
+    offset: Optional[int] = None,
+) -> str:
+    """Build a URL."""
+    base_url = f"https://www.wattpad.com/api/v3/{path}?"
+    if fields:
+        fields_str = construct_fields(fields)
+        base_url += f"fields={fields_str}&"
+
+    if limit:
+        base_url += f"limit={limit}&"
+
+    if offset:
+        base_url += f"offset={offset}&"
+
+    url = base_url.removesuffix("&")
+
+    return url
+
+
+async def fetch_url(url: str, headers: dict = {}) -> dict:
+    use_headers = base_headers.copy()
+    use_headers.update(headers)
+
+    async with aiohttp.ClientSession(headers=use_headers) as session:
+        async with session.get(url) as response:
+            return await response.json()
