b6f541de3a666e3ded08f19e716fbe9ba7a8f5e9
TheOnlyWayUp <thedarkdraconian@gmail.com>
2024-01-26 13:17:20 +0000

    feat(classes.user): Functional User Class

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,5 @@
-from models import UserModel, user_fields_T, story_fields_T
-from utils import get_fields, build_url, get_url
+from models import UserModel, UserModelFieldsType
+from utils import get_fields, build_url, fetch_url, construct_fields
 from typing import cast
 
 
@@ -13,19 +13,27 @@ class User:
     def __repr__(self) -> str:
         return f"<User username={self.username}>"
 
-    async def create(self, include: bool | list[story_fields_T] = False):
-        if include is True:
-            include_fields: list[user_fields_T] = get_fields(UserModel)  # type: ignore
-            include_fields.remove("username")
-        elif include is False:
-            include_fields: list[user_fields_T] = ["description"]
+    async def create(self, include: bool | UserModelFieldsType = False):
+        if include is False:
+            include_fields: UserModelFieldsType = {"description": True}
+        elif include is True:
+            include_fields: UserModelFieldsType = {
+                key: True for key in get_fields(UserModel)  # type: ignore
+            }
         else:
-            include_fields = cast(list[user_fields_T], include)
+            include_fields: UserModelFieldsType = include
 
-        data = await get_url(
-            build_url(f"users/{self.data.username}", fields=include_fields)  # type: ignore
+        if "username" in include_fields:
+            include_fields.pop("username")
+
+        data = await fetch_url(
+            build_url(f"users/{self.data.username}", fields=dict(include_fields))
         )
 
         self.data = UserModel(username=self.username, **data)
 
         return self
+
+    async def fetch_stories(self, include: bool | list):
+        """Populate a User's authored stories."""
+        ...
